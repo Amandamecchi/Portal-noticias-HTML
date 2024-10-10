9762b486dd0fa01738a09202ea4fd98730e6ec08
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-10-09 21:38:03 -0300

    Docs: Alterações no conteúdo - Carreiras Educação Física

--- mobile-navbar.js
+++
@@ -1,8 +0,0 @@
-class MobileNavbar{
-    constructor(mobileMenu){
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.MobileNavbar = document.querySelector(navList);
-        this.navList
-        this.navLinks
-    }
-}
